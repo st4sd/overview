d4f5a1092a3b06599cd6ab441c0febf35d04c2a7
Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>
2026-03-27 10:50:00 +0000

    docs: update instructions to update website
    Signed-off-by: Vassilis Vassiliadis <vassilis.vassiliadis@ibm.com>

--- test/check_broken_links.sh
+++ test/check_broken_links.sh
@@ -9,9 +9,33 @@ set -e
 # Start http server in the background
 gatsby serve --prefix-paths &
 
-# Download and unpack linkcheck
-wget https://github.com/filiph/linkcheck/releases/download/3.0.0/linkcheck-3.0.0-linux-x64.tar.gz
-tar -xzf linkcheck-3.0.0-linux-x64.tar.gz
+# Check if linkcheck binary exists, if not download it
+if [ ! -f "./linkcheck/linkcheck" ]; then
+    echo "linkcheck binary not found, downloading..."
+    
+    # Detect OS and set appropriate download URL
+    OS=$(uname -s)
+    if [ "$OS" = "Darwin" ]; then
+        # macOS
+        LINKCHECK_URL="https://github.com/filiph/linkcheck/releases/download/3.0.0/linkcheck-3.0.0-macos-arm64.tar.gz"
+        LINKCHECK_TAR="linkcheck-3.0.0-macos-arm64.tar.gz"
+    elif [ "$OS" = "Linux" ]; then
+        # Linux
+        LINKCHECK_URL="https://github.com/filiph/linkcheck/releases/download/3.0.0/linkcheck-3.0.0-linux-x64.tar.gz"
+        LINKCHECK_TAR="linkcheck-3.0.0-linux-x64.tar.gz"
+    else
+        echo "Unsupported operating system: $OS"
+        exit 1
+    fi
+    
+    # Download and unpack linkcheck
+    wget "$LINKCHECK_URL"
+    tar -xzf "$LINKCHECK_TAR"
+    rm "$LINKCHECK_TAR"
+    echo "linkcheck downloaded and extracted successfully"
+else
+    echo "linkcheck binary already exists, skipping download"
+fi
 
 # We need to wait a bit for the http server to be up on Travis
 sleep 30s
